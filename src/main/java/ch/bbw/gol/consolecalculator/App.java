25e58c46fbc69c8bf9e33765a23d18b97a41c7b2
gol <lorris.goyvaerts@ti8m.ch>
2018-12-21 19:12:48 +0100

    Initialized Calculator in App and calling the addition-Method

--- src/main/java/ch/bbw/gol/consolecalculator/App.java
+++ src/main/java/ch/bbw/gol/consolecalculator/App.java
@@ -1,4 +1,18 @@
 package ch.bbw.gol.consolecalculator;
 
 public class App {
+
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+        int valueA = 0;
+        int valueB = 0;
+
+        System.out.println("Console Calculator");
+        System.out.println("==================");
+        System.out.println();
+
+        valueA = 10;
+        valueB = 20;
+        System.out.println("Summe " + valueA + " + " + valueB + " = " + calculator.addition(valueA, valueB));
+    }
 }
