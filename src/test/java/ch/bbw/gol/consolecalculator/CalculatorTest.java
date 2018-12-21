4c229a83f50d40a5f0703bb0f4e540670565d60a
gol <lorris.goyvaerts@ti8m.ch>
2018-12-21 20:37:53 +0100

    Subtraction shall calculate -1 when -MIN_VALUE gets subtracted from MAX_VALUE

--- src/test/java/ch/bbw/gol/consolecalculator/CalculatorTest.java
+++ src/test/java/ch/bbw/gol/consolecalculator/CalculatorTest.java
@@ -82,6 +82,11 @@ public class CalculatorTest {
     }
 
     @Test
+    public void testSubtraktionMaxMinusMinusMinIsOk() {
+        assertTrue(calculator.subtraktion(Integer.MAX_VALUE, (-Integer.MIN_VALUE)) == (-1));
+    }
+
+    @Test
     public void testDivisionZweiPositiveIsOk() {
         assertTrue(calculator.division(6, 2) == 3);
     }
