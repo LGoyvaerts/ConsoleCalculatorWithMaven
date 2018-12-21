e34decfea31b2082ab4883f686084f6efd56f9aa
gol <lorris.goyvaerts@ti8m.ch>
2018-12-21 21:56:23 +0100

    Implemented constructor for TestCase

---
+++ src/test/java/ch/bbw/gol/consolecalculator/AppTest.java
@@ -0,0 +1,15 @@
+package ch.bbw.gol.consolecalculator;
+
+import junit.framework.TestCase;
+
+public class AppTest extends TestCase{
+
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public AppTest(String testName){
+        super(testName);
+    }
+}
