31918655f6e96135972e5118d1d4af3c14ef68b7
gol <lorris.goyvaerts@ti8m.ch>
2018-12-21 22:28:46 +0100

    Calculator subtraktion shall work in tests with visibility: protected

--- src/main/java/ch/bbw/gol/consolecalculator/Calculator.java
+++ src/main/java/ch/bbw/gol/consolecalculator/Calculator.java
@@ -6,7 +6,7 @@ public class Calculator {
         return summand1 + summand2;
     }
 
-    public int subtraktion(int value1, int value2) {
+    protected int subtraktion(int value1, int value2) {
         return value1 - value2;
     }
 
