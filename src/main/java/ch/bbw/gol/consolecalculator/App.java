d413587a02f81404fe2c81a57842ff1edc356bbd
gol <lorris.goyvaerts@ti8m.ch>
2018-12-22 11:21:35 +0100

    App shall exit on exit and run while not exit

--- src/main/java/ch/bbw/gol/consolecalculator/App.java
+++ src/main/java/ch/bbw/gol/consolecalculator/App.java
@@ -5,40 +5,52 @@ import java.util.Scanner;
 public class App {
 
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
         Calculator calculator = new Calculator();
-        String function;
-        int valueA = 0;
-        int valueB = 0;
 
-        System.out.println("Console Calculator");
-        System.out.println("==================");
-        System.out.println();
+        while (true) {
+            Scanner in = new Scanner(System.in);
 
-        System.out.println("What would you like to calculate?\n");
-        System.out.println("Plus, Minus, Times or Divide");
-        System.out.println("Please write the whole word as your answer:\n");
+            String function;
+            int valueA = 0;
+            int valueB = 0;
 
-        function = in.nextLine().toLowerCase();
+            System.out.println("Console Calculator");
+            System.out.println("==================");
+            System.out.println();
 
-        System.out.println("\n------------------\n");
-        System.out.print("This is your first value: ");
-        valueA = in.nextInt();
-        System.out.println("");
-        System.out.print("This is your second value: ");
-        valueB = in.nextInt();
-        System.out.println("\n----CALCULATING----\n");
+            System.out.println("What would you like to calculate?\n");
+            System.out.println("Plus, Minus, Times or Divide");
+            System.out.println("You can also exit by typing exit");
+            System.out.println("Please write the whole word as your answer:\n");
 
-        if (function.equals("plus")) {
-            System.out.println("Summe " + valueA + " + " + valueB + " = " + calculator.addition(valueA, valueB));
-        } else if (function.equals("minus")) {
-            System.out.println("Differenz " + valueA + " - " + valueB + " = " + calculator.subtraktion(valueA, valueB));
-        } else if (function.equals("times")) {
-            System.out.println("Multiplikation " + valueA + " * " + valueB + " = " + calculator.multiplicationTesting(valueA, valueB));
-        } else if (function.equals("divide")) {
-            System.out.println("Division " + valueA + " / " + valueB + " = " + calculator.division(valueA, valueB));
-        } else {
-            System.out.println("Please choose one of the given functions");
+            function = in.nextLine().toLowerCase();
+
+            if (function.equals("exit")) {
+                System.out.println("\nGOOD BYE");
+                break;
+            }
+
+            System.out.println("\n------------------\n");
+            System.out.print("This is your first value: ");
+            valueA = in.nextInt();
+            System.out.println("");
+            System.out.print("This is your second value: ");
+            valueB = in.nextInt();
+            System.out.println("\n----CALCULATING----\n");
+
+            if (function.equals("plus")) {
+                System.out.println("Summe " + valueA + " + " + valueB + " = " + calculator.addition(valueA, valueB));
+            } else if (function.equals("minus")) {
+                System.out.println("Differenz " + valueA + " - " + valueB + " = " + calculator.subtraktion(valueA, valueB));
+            } else if (function.equals("times")) {
+                System.out.println("Multiplikation " + valueA + " * " + valueB + " = " + calculator.multiplicationTesting(valueA, valueB));
+            } else if (function.equals("divide")) {
+                System.out.println("Division " + valueA + " / " + valueB + " = " + calculator.division(valueA, valueB));
+            } else {
+                System.out.println("Please choose one of the given functions");
+            }
+
+            System.out.println("\n==================\n");
         }
     }
 }
